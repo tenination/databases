95f034b3a44ab0e9d12bfcf9f896fc717e559f02
tenination <tim.ninan@gmail.com>
2017-10-28 12:11:17 -0500

    set up our schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,25 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT,
+  created_at DATE,
+  username VARCHAR(20),
+  roomname VARCHAR(20),
+  text VARCHAR(20)
+  
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INT,
+  name VARCHAR(20)
+  
+);
 
-
+CREATE TABLE rooms (
+  id INT,
+  name VARCHAR(20)  
+);
 
 
 /*  Execute this file from the command line by typing:
