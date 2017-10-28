46b46ebfdd22a866f9eba6373b76b516d204fd28
tenination <tim.ninan@gmail.com>
2017-10-28 12:41:08 -0500

    took a jab at db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -5,3 +5,17 @@ var mysql = require('mysql');
 // and to the database "chat".
 
 
+// our original command line command: mysql -u student < server/schema.sql
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student'
+})
+
+con.connect (function(err) {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+
+module.exports.con = con;
